e202f506aff63c94503cd85e94b85e53b09cb069
Tahsin0909 <tajisan789@gmail.com>
2023-11-08 11:52:46 +0600

    Added My Goig PAge

--- src/Components/DashBoard/MyGig/MyGig.jsx
+++ src/Components/DashBoard/MyGig/MyGig.jsx
@@ -1,12 +1,51 @@
 import { Link } from "react-router-dom";
-
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../ContextApi/ContextApi";
 
 const MyGig = () => {
+    const { User } = useContext(AuthContext)
+    const [loading, setLoading] = useState(true)
+    const [GigData, setGigData] = useState(null)
+    useEffect(() => {
+        fetch(`http://localhost:5000/gig/${User.userFirebaseUid}`)
+            .then(res => res.json())
+            .then(data => {
+                setGigData(data)
+                setLoading(false)
+            })
+            .catch(err =>{
+                setLoading(false)
+            })
+    }, [User.userFirebaseUid])
+    console.log(GigData);
     return (
         <div className='flex justify-center lg:px-32 md:px-10 px-4 pt-4 '>
-            <Link to={"/addGig"}>
-                <p className="py-40 text-xl hover:text-green-600 "><span className="text-lg">+</span>Add Gig</p>
-            </Link>
+            <div className="flex justify-center items-center py-8">
+                {
+                    loading ?
+                        <span className="loading loading-infinity text-green-900 loading-lg py-40"></span>
+                        :
+                        GigData ?
+                            <div className="card w-96 bg-base-100 shadow-xl">
+                                <figure><img src={GigData.photoUrl} alt="Shoes" /></figure>
+                                <div className="card-body">
+                                    <h2 className="card-title">
+                                    {GigData.gigTitle}
+                                        <div className="badge badge-secondary">NEW</div>
+                                    </h2>
+                                    <p>{GigData.description}</p>
+                                    <div className="card-actions justify-end">
+                                        <div className="badge badge-outline">{GigData.tag1}</div>
+                                        <div className="badge badge-outline">{GigData.tag2}</div>
+                                    </div>
+                                </div>
+                            </div>
+                            :
+                            <Link to={"/addGig"}>
+                                <p className="py-40 text-xl hover:text-green-600 "><span className="text-lg">+</span>Add Gig</p>
+                            </Link>
+                }
+            </div >
         </div>
     );
 };
--- src/Components/DashBoard/MyJobPost/MyJobPost.jsx
+++ src/Components/DashBoard/MyJobPost/MyJobPost.jsx
@@ -1,5 +1,6 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../ContextApi/ContextApi";
+import { Link } from "react-router-dom";
 
 
 const MyJobPost = () => {
@@ -21,46 +22,51 @@ const MyJobPost = () => {
                 loading ?
                     <span className="loading loading-infinity text-green-900 loading-lg py-40"></span>
                     :
-                    <div>
-                        <div className="overflow-x-auto">
-                            <table className="table">
-                                {/* head */}
-                                <thead>
-                                    <tr className="text-green-900">
-                                        <th>Posted Email</th>
-                                        <th>Job Title</th>
-                                        <th>Job Category</th>
-                                        <th>Min Price</th>
-                                        <th>Max Price</th>
-                                        <th>Description</th>
-                                        <th>Posted Date</th>
-                                        <th>Deadline</th>
-                                        <th>Action</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {
-                                        MyJobPostData?.map(data => <tr className="hover" key={data._id}>
-                                            {/* row 1 */}
-                                            <td>{data?.email}</td>
-                                            <td>{data?.jobTitle}</td>
-                                            <td>{data?.jobCategory}</td>
-                                            <td>{data?.minPrice}</td>
-                                            <td>{data?.maxPrice}</td>
-                                            <td>{data?.description.slice(0, 200)}</td>
-                                            <td>{data?.postedDate}</td>
-                                            <td>{data?.deadLine}</td>
-                                            <td className="flex flex-col items-center gap-1">
-                                                <button className="py-1 px-2 w-24 border-2 hover:bg-green-900 hover:text-white border-green-900 bg-white text-green-900 ">Delete</button>
-                                                <button className="py-1 px-2 w-24 border-2 hover:bg-green-900 hover:text-white border-green-900 bg-white text-green-900 ">Update</button>
-                                            </td>
+                    MyJobPostData.length > 0 ?
+                        <div>
+                            <div className="overflow-x-auto">
+                                <table className="table">
+                                    {/* head */}
+                                    <thead>
+                                        <tr className="text-green-900">
+                                            <th>Posted Email</th>
+                                            <th>Job Title</th>
+                                            <th>Job Category</th>
+                                            <th>Min Price</th>
+                                            <th>Max Price</th>
+                                            <th>Description</th>
+                                            <th>Posted Date</th>
+                                            <th>Deadline</th>
+                                            <th>Action</th>
                                         </tr>
-                                        )
-                                    }
-                                </tbody>
-                            </table>
+                                    </thead>
+                                    <tbody>
+                                        {
+                                            MyJobPostData?.map(data => <tr className="hover" key={data._id}>
+                                                {/* row 1 */}
+                                                <td>{data?.email}</td>
+                                                <td>{data?.jobTitle}</td>
+                                                <td>{data?.jobCategory}</td>
+                                                <td>{data?.minPrice}</td>
+                                                <td>{data?.maxPrice}</td>
+                                                <td>{data?.description.slice(0, 200)}</td>
+                                                <td>{data?.postedDate}</td>
+                                                <td>{data?.deadLine}</td>
+                                                <td className="flex flex-col items-center gap-1">
+                                                    <button className="py-1 px-2 w-24 border-2 hover:bg-green-900 hover:text-white border-green-900 bg-white text-green-900 ">Delete</button>
+                                                    <button className="py-1 px-2 w-24 border-2 hover:bg-green-900 hover:text-white border-green-900 bg-white text-green-900 ">Update</button>
+                                                </td>
+                                            </tr>
+                                            )
+                                        }
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
-                    </div>
+                        :
+                        <Link to={"/addGig"}>
+                            <p className="py-40 text-xl hover:text-green-600 "><span className="text-lg">+</span>Post A Job</p>
+                        </Link>
             }
         </div >
     );
